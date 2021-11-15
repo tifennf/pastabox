b9b24611cbce5b5dcaeb94b6a872cc13cad868ef
Tifenn <tifenn.fl@gmail.com>
2021-11-15 18:42:53 +0100

    temp

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,29 @@
-use clipboard_win::set_clipboard_string;
-use eframe::{egui, epi};
+use std::{
+    sync::{mpsc, Mutex},
+    thread,
+    time::Duration,
+};
 
+use clipboard_win::{get_clipboard_string, set_clipboard_string};
+use eframe::{egui, epi};
+use windows::{ApplicationModel::DataTransfer::Clipboard, Foundation::EventHandler};
+
+type PastaList = Mutex<Vec<String>>;
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
 pub struct PastaBox {
+    last_pasta: Mutex<String>,
     new_pasta: String,
-    pastabox: Vec<String>,
+    pastabox: PastaList,
 }
 
 impl Default for PastaBox {
     fn default() -> Self {
         Self {
+            last_pasta: Mutex::new(String::new()),
             new_pasta: String::new(),
-            pastabox: Vec::new(),
+            pastabox: Mutex::new(Vec::new()),
         }
     }
 }
@@ -44,10 +54,8 @@ impl epi::App for PastaBox {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
-        let Self {
-            new_pasta,
-            pastabox,
-        } = self;
+        let new_pasta = &mut self.new_pasta;
+        let pastabox = &mut self.pastabox;
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
@@ -56,21 +64,24 @@ impl epi::App for PastaBox {
                 ui.text_edit_singleline(new_pasta);
 
                 if add_button.clicked() {
-                    pastabox.push(new_pasta.clone());
-                    *new_pasta = String::new();
+                    let guard = pastabox.lock();
+
+                    if let Ok(mut list) = guard {
+                        list.push(new_pasta.clone());
+                        *new_pasta = String::new();
+                    }
                 }
             })
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            pastabox
-                .clone()
-                .iter()
-                .enumerate()
-                .for_each(|(index, pasta)| {
+            let guard = pastabox.lock();
+
+            if let Ok(mut list) = guard {
+                list.clone().iter().enumerate().for_each(|(index, pasta)| {
                     ui.horizontal(|ui| {
                         let copy_button = ui.button("COPY");
-                        let del_button = ui.button("DEL");
+                        let del_button = ui.small_button("DEL");
 
                         ui.label(pasta);
 
@@ -79,12 +90,34 @@ impl epi::App for PastaBox {
                         }
 
                         if del_button.clicked() {
-                            pastabox.remove(index);
+                            list.remove(index);
                         }
                     });
                 });
+            }
 
             egui::warn_if_debug_build(ui);
         });
     }
+}
+
+fn seek_pastas(pastabox: &'static PastaList, last_clipboard: Mutex<String>) {
+    thread::spawn(move || loop {
+        let pasta = get_clipboard_string();
+
+        // match (pasta,)
+
+        // if let Some(last) = &last_clipboard {
+        //     if let Ok(pasta) = pasta {
+        //         if &pasta != last {
+        //             let guard = pastabox.lock();
+        //             if let Ok(mut list) = guard {
+        //                 list.push(pasta);
+        //             }
+        //         }
+        //     }
+        // }
+
+        thread::sleep(Duration::from_secs(5));
+    });
 }
