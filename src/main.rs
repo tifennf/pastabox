74b5abe4a82cff9b5f95d1dc619bc96a92602608
Tifenn <tifenn.fl@gmail.com>
2021-11-17 00:51:24 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,7 @@
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    // use windows::{ApplicationModel::DataTransfer::Clipboard, Foundation::EventHandler};
-
-    // let event = EventHandler::new(|_a, _b| {
-    //     println!("xd");
-
-    //     Ok(())
-    // });
-
-    // Clipboard::ContentChanged(event).unwrap();
-
+    
     let app = pastabox::PastaBox::default();
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(Box::new(app), native_options);
